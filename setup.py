dcda8f6f92987be279a9de8989c6dd41c470f86c
alex <alex.lundberg@sandc.com>
2018-08-28 13:05:34 -0500

    release 0.0.5

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="LeshanRestAPI",
-    version="0.0.5",
+    version="0.0.6",
     author="Alex Lundberg",
     author_email="alex.lundberg@gmail.com",
     description="Wrapper for Leshan IOT RESTful API",
